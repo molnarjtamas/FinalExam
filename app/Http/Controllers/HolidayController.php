749bd2967b077baee538d8b4dc687dfbffde1e75
Tamas <mj.tamas@protonmail.com>
2021-03-04 17:01:55 +0200

    conflict solve

--- app/Http/Controllers/HolidayController.php
+++ app/Http/Controllers/HolidayController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
+
 use App\Http\Resources\HolidayResource;
 use App\Models\Holiday;
-=======
->>>>>>> add-holiday-feature
 use Illuminate\Http\Request;
 
 class HolidayController extends Controller
 {
-<<<<<<< HEAD
+
     public function index()
     {
         return HolidayResource::collection(Holiday::orderBy('start_date')->paginate(10));
 
     }
-=======
-    //
->>>>>>> add-holiday-feature
+
 }
